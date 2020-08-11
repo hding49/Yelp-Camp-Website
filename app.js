01ec07242ba0ade1930de7fd5445f9632249e721
hding49 <hding49@uwo.ca>
2020-08-11 15:33:49 -0400

    update mongoose

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+// mongoose.connect("mongodb://localhost/yelp_camp_v9");
+mongoose.connect('mongodb+srv://haoran:yelpcamp123@cluster0.i6pcy.mongodb.net/yelpcamp?retryWrites=true&w=majority', { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
